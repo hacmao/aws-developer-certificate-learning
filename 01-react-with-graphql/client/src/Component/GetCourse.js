8ea43be4b55387f0554cccc9c0e62a8320e505cb
hacmao <nguyentuanhieptuki@gmail.com>
2021-02-02 20:50:42 +0700

    update

--- 01-react-with-graphql/client/src/Component/GetCourse.js
+++ 01-react-with-graphql/client/src/Component/GetCourse.js
@@ -1,20 +1,24 @@
 import { useQuery } from '@apollo/client';
 import { LOAD_COURSE } from '../graphql/getCourse'
 import Course from './Course';
+import { useEffect, useState } from 'react';
 
 const GetCourses = () => {
+  const [course, setCourse] = useState([]);
+  
   const {loading, error, data} = useQuery(LOAD_COURSE);
- 
+  
   if (loading) return <p>Loading ...</p>;
   if (error) throw error;
   if (data)
-    console.log(data.courses);
-    
+    console.log(data);
+
   return data.courses.map(({title, author, description}) => (
     <Course 
       title={title}
       author={author}
       description={description}
+      key={title}
     />
   ));
 }
